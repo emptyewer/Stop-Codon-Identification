7a275fffafc3198fed5f1506b02748054cc4cb36
emptyewer <venky.agas@gmail.com>
2015-05-13 16:19:40 -0500

    added HEK 293 analysis

--- parse_cdna_library.py
+++ parse_cdna_library.py
@@ -1,4 +1,4 @@
-import re, sys, time
+import re, sys, datetime, os
 import httplib, textwrap, types
 import subprocess
 from shlex import split
@@ -7,7 +7,28 @@ from Bio import Entrez
 from Bio import UniGene
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
-from xenopus.models import Gene
+
+tax_id = sys.argv[1]
+expression_tag_filename = ''
+unigene_db_filename = ''
+output_filename = ''
+
+#Add/Change this section for adding more organisms
+if tax_id == '8355':
+    from xenopus.models import Gene
+    expression_tag_filename = "XL_cDNA_5660.txt"
+    unigene_db_filename = "Xl.data"
+    output_filename = "results_xenopus.txt"
+    if os.path.exists(output_filename):
+        output_filename = output_filename[-4] + str(datetime.time()) + ".txt"
+elif tax_id == '9606':
+    from hek293.models import Gene
+    expression_tag_filename = "HS_cDNA_16145.txt"
+    unigene_db_filename = "Hs.data"
+    output_filename = "results_hek293.txt"
+    if os.path.exists(output_filename):
+        output_filename = output_filename[-4] + str(datetime.time()) + ".txt"
+
 
 Entrez.email = "vkrishnamani@healthcare.uiowa.edu"
 
@@ -49,14 +70,15 @@ def get_protsim(record):
                     largest_similarity = float(split[1])
         xenopus_protein[key] = protsim_dict
 
-    if '8355' in xenopus_protein.keys():
-        return xenopus_protein['8355']
+    if tax_id in xenopus_protein.keys():
+        return xenopus_protein[tax_id]
     else:
         for key in xenopus_protein.keys():
             if float(xenopus_protein[key]['PCT']) == largest_similarity:
                 return xenopus_protein[key]
 
-records = SeqIO.parse("XL_cDNA_5660.txt", "fasta")
+records = SeqIO.parse(expression_tag_filename, "fasta")
+print "Est db", expression_tag_filename
 match_unigene_id = re.compile("/ug=(.+)")
 est_records_count = 0
 unigene_ids = []
@@ -83,20 +105,24 @@ def get_nucleotide_record(gene_id):
     gene_sequence_record = Entrez.read(handle)
     handle.close()
     return gene_sequence_record
-output = open("results_xenopus.txt", 'w')
-input = open("Xl_cleaned.data")
-records = UniGene.parse(input)
-for record in records:
+output = open(output_filename, 'w')
+unigene_input = open(unigene_db_filename)
+print "Unigene db", unigene_db_filename
+unigene_records = UniGene.parse(unigene_input)
+processed = 1
+for record in unigene_records:
     if record.ID in unique_unigene_ids and not Gene.objects.filter(unigene_id=record.ID).exists():
         db_gene = Gene()
+        db_gene.taxonomy_id = tax_id
         output.write("="*30)
         output.write("\n")
         output.write("UniGene ID : %s\n" % record.ID)
-        print "Accessing Record Unigene : ", record.ID
+        print "Accessing Unigene Record... ", record.ID, " (", processed, " of ", est_records_count, ")"
         db_gene.unigene_id = record.ID
         protsim = get_protsim(record)
         if type(protsim) != types.NoneType:
             protein_record = ''
+            db_gene.protein_similarity_taxonomy_id = protsim['ORG']
             try:
                 protein_record = get_protein_record(protsim['PROTID'])
             except httplib.BadStatusLine:
@@ -108,7 +134,7 @@ for record in records:
             output.write("Protein Length : %s\n" % protein_record[0]['GBSeq_length'])
             db_gene.protein_length = protein_record[0]['GBSeq_length']
             output.write("Protein Source Organism : %s\n" % protein_record[0]['GBSeq_source'])
-            db_gene.organism = protein_record[0]['GBSeq_source']
+            db_gene.source_organism = protein_record[0]['GBSeq_source']
             output.write("Protein Name : %s\n" % protein_record[0]['GBSeq_definition'])
             db_gene.protein_name = protein_record[0]['GBSeq_definition']
             protein_sequence = protein_record[0]['GBSeq_sequence'].upper()
@@ -180,5 +206,6 @@ for record in records:
             db_gene.protein_sequence = "TRANSCRIBED LOCUS BUT NO MATCHING GENE FOUND"
             output.write("TRANSCRIBED LOCUS BUT NO MATCHING GENE FOUND\n")
         db_gene.save()
-input.close()
+        processed += 1
+unigene_input.close()
 output.close()
--- xenopus/models.py
+++ xenopus/models.py
@@ -2,30 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Gene(models.Model):
+    taxonomy_id = models.PositiveIntegerField(default=0)
+    source_organism = models.CharField(max_length=200)
+
     unigene_id = models.CharField(max_length=20)
     protein_id = models.CharField(max_length=20)
-    gene_id = models.CharField(max_length=20)
-
-    protein_length = models.PositiveIntegerField(default=0)
-    gene_length = models.PositiveIntegerField(default=0)
-
     protein_name = models.CharField(max_length=200)
-    gene_name = models.CharField(max_length=200)
-
-    organism = models.CharField(max_length=200)
-    gene_type = models.CharField(max_length=20)
-
+    protein_length = models.PositiveIntegerField(default=0)
+    protein_similarity_taxonomy_id = models.PositiveIntegerField(default=0)
     protein_similarity = models.FloatField(max_length=5, default=0)
     protein_classification = models.CharField(max_length=10)
     protein_tm_helices = models.PositiveIntegerField(default=0)
     protein_predicted_segments = models.TextField()
+    protein_start_aa = models.CharField(max_length=1)
+    protein_stop_aa = models.CharField(max_length=1)
+    protein_sequence = models.TextField()
 
+    gene_id = models.CharField(max_length=20)
+    gene_name = models.CharField(max_length=200)
+    gene_type = models.CharField(max_length=20)
+    gene_length = models.PositiveIntegerField(default=0)
     gene_start_nucleotide = models.PositiveIntegerField(default=0)
     gene_end_nucleotide = models.PositiveIntegerField(default=0)
     gene_start_codon = models.CharField(max_length=3)
     gene_stop_codon = models.CharField(max_length=3)
-    protein_start_aa = models.CharField(max_length=1)
-    protein_stop_aa = models.CharField(max_length=1)
-
     gene_sequence = models.TextField()
-    protein_sequence = models.TextField()
+
